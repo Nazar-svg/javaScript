075a41e6b8dc9ebcd256d2b06db82995a3bb287d
Nazar <nazar.kolos90@gmail.com>
2020-02-10 20:29:35 +0200

    Start

---
+++ app.js
@@ -0,0 +1,22 @@
+let age = 35 ;
+let name;
+
+name = 25;
+
+let wight = 100;
+
+age = wight;
+
+console.log("name=", 25);
+
+console.log(age+name+wight);
+
+console.log(age/(name+wight));
+
+console.log(wight*age+name);
+
+
+
+
+
+
