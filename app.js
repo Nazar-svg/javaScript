4506bee9b3b25f1de76c14dabb6f8380baa0aaa8
Nazar <nazar.kolos90@gmail.com>
2020-03-06 23:14:32 +0200

    +++++++++

--- app.js
+++ app.js
@@ -304,13 +304,25 @@
 
 
 
-// приклад з використанням  стрілочної функції на вирахунок найбільшого ек чщтного  числа ч
+// // приклад з використанням  стрілочної функції на вирахунок найбільшого ек чщтного  числа ч
 
-const arr = ['1','2','3','4'];
+// const arr = ['1','2','3','4'];
 
-const res = arr
-.map((el) => parseInt(el))
-.filter((num) => num%2)
-.reduce((max, value) => Math.max(max, value), 0);
+// const res = arr
+// .map((el) => parseInt(el))
+// .filter((num) => num%2)
+// .reduce((max, value) => Math.max(max, value), 0);
 
-console.log(res);
+// console.log(res);
+
+// __________ДЕСТРУКТЕРИЗАЦІЯ ОБЄКТІВ______________
+
+const dict = {
+  duck:'ква',
+  dog:'гав',
+  mouse:'пипи'
+}
+
+const { duck, ...other } = dict;
+
+console.log(duck,other);
